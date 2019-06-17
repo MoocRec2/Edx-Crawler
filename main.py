3b580e6dda97451f1800df7cf679097646cff5d6
Sameer Mohamed <smrbasil4@gmail.com>
2019-06-17 23:31:48 +0530

    Changed Retrieve Thread Info into a Method and called it in main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from retrieve_thread_info import retrieve_thread_info
+
+course_one = 'https://courses.edx.org/courses/course-v1:Microsoft+DAT236x+1T2019a/course/'
+course_two = 'https://courses.edx.org/courses/course-v1:UCSanDiegoX+DSE200x+1T2019a/course/'
+course_three = 'https://courses.edx.org/courses/course-v1:UPValenciaX+TGV201x.2+2T2019/course/'
+
+retrieve_thread_info(course_one)
