fb8d0bbc678eee39da54495b3a5fb5e6e3f27b07
Sameer Mohamed <smrbasil4@gmail.com>
2019-06-24 20:09:19 +0530

    Implemented Saving Subject Data to the Database

--- retrieve_subjects.py
+++ retrieve_subjects.py
@@ -2,10 +2,12 @@ import time
 from selenium import webdriver
 import requests
 from local_storage import LocalStorage
+from pprint import pprint
+from db_connector import Thread, Course, Subject
 
 # Optional argument, if not specified will search path.
 driver = webdriver.Chrome('C:/chromedriver')
-
+print('Authenticating')
 # Sign in Page
 driver.get('https://courses.edx.org/login')
 email_input = driver.find_element_by_name("email")
@@ -16,6 +18,8 @@ password_input.send_keys("nvidia1024")
 
 login_button = driver.find_element_by_class_name('login-button')
 login_button.click()
+
+print('Authenticated')
 
 cookies = driver.get_cookies()
 driver.quit()
@@ -30,5 +34,12 @@ r = requests.get(subjects_url, cookies=cookies_dict)
 
 response_data = r.json()
 
-for subject in response_data['results']:
-    print(subject['name'])
+print('Retrieved {} Subjects'.format(response_data['results'].__len__()))
+
+# for subject in response_data['results']:
+#     # print(subject['name'])
+#     pprint(subject)
+#     break
+
+Subject.upsert_subjects(response_data['results'])
+print('Subject Records have been saved to the Database')
