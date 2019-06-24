50e5e1c96345f9681bd034c75481d74767538cbf
Sameer Mohamed <smrbasil4@gmail.com>
2019-06-24 19:56:30 +0530

    Implemented Iteration of Courses

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from retrieve_thread_info import retrieve_thread_info
+from db_connector import Course
+from pprint import pprint
 
 course_one = 'https://courses.edx.org/courses/course-v1:Microsoft+DAT236x+1T2019a/course/'
 course_two = 'https://courses.edx.org/courses/course-v1:UCSanDiegoX+DSE200x+1T2019a/course/'
 course_three = 'https://courses.edx.org/courses/course-v1:UPValenciaX+TGV201x.2+2T2019/course/'
+course_four = 'https://courses.edx.org/courses/course-v1:HarvardX+PH125.8x+2T2018/course/'
 
-retrieve_thread_info(course_one)
+# courses = Course.get_courses()
+# for course in courses:
+#     pprint(course)
+#     retrieve_thread_info(course['marketing_url'])
+
+retrieve_thread_info(course_four)
