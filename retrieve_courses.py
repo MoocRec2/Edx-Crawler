31356cd12e7ae8494b24c2a93811a43697e2473e
Sameer Mohamed <smrbasil4@gmail.com>
2019-06-24 19:55:19 +0530

    Improved Course Retrieving Script

--- retrieve_courses.py
+++ retrieve_courses.py
@@ -12,14 +12,21 @@ courses_url = 'https://www.edx.org/api/v1/catalog/search?selected_facets[]=subje
 
 next_url = courses_url
 count = 1
+course_count = 0
 
 while True:
     print('Iteration =', count)
+    if next_url is None:
+        break
     r = requests.get(next_url, cookies=cookies)
     response_data = r.json()
     courses = response_data['objects']['results']
     result = Course.upsert_courses(courses)
     if result:
-        print('Saved/Updated', courses.__len__(), ' Courses')
+        courses_len = courses.__len__()
+        print('Saved/Updated', courses_len, ' Courses')
+        course_count += courses_len
     next_url = response_data['objects']['next']
     count += 1
+
+print('Retrieved {} Courses'.format(course_count))
