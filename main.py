a2271d4e751744ac34d3c58abeaf04c1ca92c341
Sameer Mohamed <smrbasil4@gmail.com>
2019-08-31 23:08:45 +0530

    FIxed Iteration Error and Added Time Elapsed Logging

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from retrieve_thread_info import retrieve_thread_info
 import db_connector
 from pprint import pprint
+import time
 
 '''
     MAIN Script
     
     Process:
-    1 - Get Courses
+    1 - Get Courses from Database
     2 - Iterate Courses and Get their Threads
+    
+    Note: Courses retrieving logic exists in the retrieve_courses.py script
 '''
+
+start_time = time.time()
 
 courses_alt = list(db_connector.Course.get_edx_courses())
 
@@ -23,9 +28,16 @@ num = int(num)
 
 count = 0
 driver = None
-for course in range(num):
+
+tracking_array = list(range(num))
+
+for x in tracking_array:
     count += 1
-    print('------ Iteration', count, '------')
-    driver = retrieve_thread_info(courses[course], driver)
+    print('------ Course No.:', count, '------')
+    driver = retrieve_thread_info(courses[x % courses.__len__()], driver)
     is_authenticated = True
     print('------- Overall progress:', (count / total) * 100, '%')
+
+end_time = time.time()
+time_elapsed = end_time - start_time
+print('Time Elapsed:', time_elapsed, 'seconds')
